7c2ecd0583ded6b5cd58a43e3111b98dc448f2d0
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-02-20 09:03:43 +0100

    feat: add Toolbar and remove legacy components (#1788)

--- packages/frontend/tests/stories/searchSenders.spec.ts
+++ packages/frontend/tests/stories/searchSenders.spec.ts
@@ -5,16 +5,18 @@ test.describe('Search suggests with senders', () => {
   test('Render suggestions with senders', async ({ page }) => {
     await page.goto(`${defaultAppURL}&playwrightId=search-sender`);
 
-    await page.getByPlaceholder('Søk').click();
-    await page.getByPlaceholder('Søk').fill('arbeids');
+    const searchbarInput = page.locator("[name='Søk']");
+    await expect(searchbarInput).toBeVisible();
+    await searchbarInput.click();
+    await searchbarInput.fill('arbeids');
     await expect(page.getByRole('heading', { name: 'Søkeforslag' })).toBeVisible();
     await expect(page.getByRole('button', { name: 'Arbeids- og velferdsetaten (NAV)' })).toBeVisible();
 
-    await page.getByPlaceholder('Søk').fill('skatt');
+    await searchbarInput.fill('skatt');
     await expect(page.getByRole('heading', { name: 'Søkeforslag' })).toBeVisible();
     await expect(page.getByRole('button', { name: 'Skatteetaten' })).toBeVisible();
 
-    await page.getByPlaceholder('Søk').fill('skatt test1');
+    await searchbarInput.fill('skatt test1');
     await expect(page.getByRole('heading', { name: 'Søkeforslag' })).toBeVisible();
     const button = page.locator('button[aria-label="Skatteetaten"]');
     await expect(button.locator(':text("test1")')).toBeVisible();
@@ -23,8 +25,9 @@ test.describe('Search suggests with senders', () => {
   test('Not rendering senders suggestions with no match', async ({ page }) => {
     await page.goto(`${defaultAppURL}&playwrightId=search-sender`);
 
-    await page.getByPlaceholder('Søk').click();
-    await page.getByPlaceholder('Søk').fill('test1');
+    const searchbarInput = page.locator("[name='Søk']");
+    await searchbarInput.click();
+    await searchbarInput.fill('test1');
 
     await expect(page.getByRole('heading', { name: 'Søkeforslag' })).not.toBeVisible();
   });
@@ -32,8 +35,9 @@ test.describe('Search suggests with senders', () => {
   test('Selecting sender search filters data correctly', async ({ page }) => {
     await page.goto(`${defaultAppURL}&playwrightId=search-sender`);
 
-    await page.getByPlaceholder('Søk').click();
-    await page.getByPlaceholder('Søk').fill('skatt');
+    const searchbarInput = page.locator("[name='Søk']");
+    await searchbarInput.click();
+    await searchbarInput.fill('skatt');
     await page.getByRole('button', { name: 'Skatteetaten' }).click();
     await expect(page).toHaveURL(`${defaultAppURL}&playwrightId=search-sender&org=skd`);
 
@@ -46,8 +50,9 @@ test.describe('Search suggests with senders', () => {
   test('Selecting sender and value filters data correctly', async ({ page }) => {
     await page.goto(`${defaultAppURL}&playwrightId=search-sender`);
 
-    await page.getByPlaceholder('Søk').click();
-    await page.getByPlaceholder('Søk').fill('skatt test1');
+    const searchbarInput = page.locator("[name='Søk']");
+    await searchbarInput.click();
+    await searchbarInput.fill('skatt test1');
     await page.getByRole('button', { name: 'Skatteetaten' }).click();
 
     await expect(page).toHaveURL(`${defaultAppURL}&playwrightId=search-sender&search=test1&org=skd`);
@@ -60,8 +65,9 @@ test.describe('Search suggests with senders', () => {
   test('Clear button removes search parasm and display data', async ({ page }) => {
     await page.goto(`${defaultAppURL}&playwrightId=search-sender`);
 
-    await page.getByPlaceholder('Søk').click();
-    await page.getByPlaceholder('Søk').fill('skatt test1');
+    const searchbarInput = page.locator("[name='Søk']");
+    await searchbarInput.click();
+    await searchbarInput.fill('skatt test1');
     await page.getByRole('button', { name: 'Skatteetaten' }).click();
 
     await expect(page).toHaveURL(`${defaultAppURL}&playwrightId=search-sender&search=test1&org=skd`);
