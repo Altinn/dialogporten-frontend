98070c6b437c73936e52ca304f7455c55a792633
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-02-21 16:36:51 +0100

    feat: add dialog header and fix wrapper for dialog details page

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './i18n/config.ts';
-
-import '@digdir/designsystemet-css';
 import '@digdir/designsystemet-theme';
 
 import { RootProvider } from '@altinn/altinn-components';
