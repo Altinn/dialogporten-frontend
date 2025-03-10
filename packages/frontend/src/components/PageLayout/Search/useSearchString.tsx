19bf05e947856b54224fc5bc8f30da452d7e1e14
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-03-10 10:53:10 +0100

    refactor: introduce useGlobalState as cleaner shared state utility with a simple API (#1921)

--- packages/frontend/src/components/PageLayout/Search/useSearchString.tsx
+++ packages/frontend/src/components/PageLayout/Search/useSearchString.tsx
@@ -1,31 +1,19 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { QUERY_KEYS } from '../../../constants/queryKeys.ts';
 import { getSearchStringFromQueryParams } from '../../../pages/Inbox/queryParams.ts';
 import { PageRoutes } from '../../../pages/routes.ts';
+import { useGlobalState } from '../../../useGlobalState.ts';
 
 export const useSearchString = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const location = useLocation();
   const navigate = useNavigate();
   const searchQueryParams = getSearchStringFromQueryParams(searchParams);
-  const queryClient = useQueryClient();
+  const [searchValue, setSearchValue] = useGlobalState(QUERY_KEYS.SEARCH_VALUE, searchQueryParams);
+  const [enteredSearchValue, setEnteredSearchValue] = useGlobalState(QUERY_KEYS.ENTERED_SEARCH_VALUE, '');
 
-  const { data: searchValue } = useQuery<string>({
-    queryKey: [QUERY_KEYS.SEARCH_VALUE],
-    enabled: false,
-    staleTime: Number.POSITIVE_INFINITY,
-    initialData: searchQueryParams,
-  });
-
-  const { data: enteredSearchValue } = useQuery<string>({
-    queryKey: [QUERY_KEYS.ENTERED_SEARCH_VALUE],
-    enabled: false,
-    staleTime: Number.POSITIVE_INFINITY,
-    initialData: '',
-  });
-
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     const searchBarParam = new URLSearchParams(searchParams);
     const searchParamValue = searchBarParam.get('search') ?? '';
@@ -35,14 +23,6 @@ export const useSearchString = () => {
       setEnteredSearchValue(searchParamValue);
     }
   }, [searchParams, enteredSearchValue]);
-
-  const setSearchValue = (value: string) => {
-    queryClient.setQueryData([QUERY_KEYS.SEARCH_VALUE], value);
-  };
-
-  const setEnteredSearchValue = (value: string) => {
-    queryClient.setQueryData([QUERY_KEYS.ENTERED_SEARCH_VALUE], value);
-  };
 
   const onSearch = (value: string, org?: string) => {
     const newSearchParams = new URLSearchParams(searchParams);
