1bf92171e48cca1da102b1781d2097f944101a47
Mateusz Bacherycz <mbacherycz@icloud.com>
2025-04-28 13:53:36 +0200

    feat: initial profile landing page layout

--- packages/frontend/src/pages/Profile/Profile.tsx
+++ packages/frontend/src/pages/Profile/Profile.tsx
@@ -1,8 +1,12 @@
-import { PageBase, PageNav } from '@altinn/altinn-components';
+import { Button, DashboardCard, DashboardHeader, Flex, PageBase, PageNav, Typography } from '@altinn/altinn-components';
 import { t } from 'i18next';
 import { PageRoutes } from '../routes';
+// import { useProfile } from '../../profile';
 
 export const Profile = () => {
+  // const { profile } = useProfile();
+
+  // console.log('Profile', profile);
   return (
     <PageBase>
       <PageNav
@@ -17,7 +21,51 @@ export const Profile = () => {
           },
         ]}
       />
-      <h1>{t('sidebar.profile')}</h1>
+
+      <DashboardHeader name="Ronald McDonald" description="Fødselsnummer: 26.06.1966 XX.XX.XX">
+        <Typography>
+          <h6>E-post</h6>
+          <p>mathias.dyngeland@brann.no</p>
+          <h6>Telefon</h6>
+          <p>55 00 00 00</p>
+          <h6>Adresse</h6>
+          <p>Idrettsveien 1, 5020 Bergen</p>
+        </Typography>
+        <Flex justify="start" spacing={2}>
+          <Button variant="outline" size="sm" onClick={() => {}}>
+            Endre telefon/e-post
+          </Button>
+          <Button variant="outline" size="sm" onClick={() => {}}>
+            Endre adresse
+          </Button>
+        </Flex>
+      </DashboardHeader>
+      <Flex spacing={4}>
+        <DashboardCard color="person">
+          <h1>heeeeelooo</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus, sit amet dictum leo
+            consectetur in. Maecenas in libero euismod, facilisis ligula ut, efficitur nunc. Donec a diam lectus, sit
+            amet dictum leo consectetur in. Maecenas in libero euismod,{' '}
+          </p>
+        </DashboardCard>
+        <DashboardCard color="person">
+          <h1>heeeeelooo</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus, sit amet dictum leo
+            consectetur in. Maecenas in libero euismod, facilisis ligula ut, efficitur nunc. Donec a diam lectus, sit
+            amet dictum leo consectetur in. Maecenas in libero euismod,{' '}
+          </p>
+        </DashboardCard>
+        <DashboardCard color="person">
+          <h1>heeeeelooo</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus, sit amet dictum leo
+            consectetur in. Maecenas in libero euismod, facilisis ligula ut, efficitur nunc. Donec a diam lectus, sit
+            amet dictum leo consectetur in. Maecenas in libero euismod,{' '}
+          </p>
+        </DashboardCard>
+      </Flex>
     </PageBase>
   );
 };
