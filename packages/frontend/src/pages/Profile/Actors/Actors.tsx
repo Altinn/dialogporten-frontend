00bb4141cf2fbff74c7b9a16c9f1df9537bb3a98
Alexander Midteide <alexander.midteide@digdir.no>
2025-05-01 10:50:52 +0200

    feat: Added profile data to Actor page.

--- packages/frontend/src/pages/Profile/Actors/Actors.tsx
+++ packages/frontend/src/pages/Profile/Actors/Actors.tsx
@@ -1,24 +1,68 @@
-import { DsAlert, DsChip, ListBase, PageBase, PageNav, Toolbar, Typography } from '@altinn/altinn-components';
+import {
+  type AccountListItemType,
+  type AvatarSize,
+  type AvatarType,
+  Button,
+  type Color,
+  DsParagraph,
+  Heading,
+  ListBase,
+  PageBase,
+  PageNav,
+  Section,
+  TextField,
+  Toolbar,
+  Typography,
+} from '@altinn/altinn-components';
+import { DsSwitch } from '@altinn/altinn-components';
+import { HeartFillIcon, HeartIcon, InboxIcon, PlusIcon } from '@navikt/aksel-icons';
+import type { PartyFieldsFragment } from 'bff-types-generated';
 import { t } from 'i18next';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useParties } from '../../../api/hooks/useParties';
 import { useProfile } from '../../../profile';
 import { PageRoutes } from '../../routes';
 import { PartyListItem } from './ActorListItem';
 import styles from './actors.module.css';
 
+const breadcrumbs = [
+  {
+    label: t('word.frontpage'),
+    href: PageRoutes.inbox,
+  },
+  {
+    label: t('sidebar.profile'),
+    href: PageRoutes.profile,
+  },
+  {
+    label: t('sidebar.profile.actors'),
+    href: PageRoutes.actors,
+  },
+];
+
 export const Actors = () => {
   const [searchValue, setSearchValue] = useState('');
-  const [showSubActors, setShowSubActors] = useState(false);
-  const [showDeletedActors, setShowDeletedActors] = useState(false);
-  const { favoriteActors, user } = useProfile();
+  const showDeletedActors = false; // TODO: Implement toolbar filter for deleted actors
+  const [isPreselectedActor, setIsPreselectedActor] = useState(false);
+  const { favoriteActors, profile, user, toggleFavoriteActor } = useProfile();
+  const navigate = useNavigate();
 
   // TODO: Remove this when we have implemented the new profile API
+  console.info('Got profile from Core Platform API: ', profile);
   console.info('Got user from Core Platform API: ', user);
 
   const { parties: normalParties, isLoading: isLoadingParties, deletedParties } = useParties();
 
-  const parties = showDeletedActors ? [...normalParties, ...deletedParties] : normalParties;
+  const getPartiesToShow = () => {
+    const partiesSource = showDeletedActors ? [...normalParties, ...deletedParties] : normalParties;
+    if (searchValue) {
+      return partiesSource.filter((party) => party.name.toLowerCase().includes(searchValue.toLowerCase()));
+    }
+    return partiesSource;
+  };
+
+  const parties = getPartiesToShow();
   if (isLoadingParties) {
     return (
       <div className={styles.noResults}>
@@ -26,34 +70,119 @@ export const Actors = () => {
       </div>
     );
   }
-  const favoriteParties = parties.filter((party) => favoriteActors?.find((actor) => actor?.includes(party.party)));
-  const nonFavoriteParties = parties.filter((party) => !favoriteActors?.find((actor) => actor?.includes(party.party)));
+  const favoriteParties = parties.filter((party) => {
+    if (favoriteActors?.find((actor) => actor?.includes(party.party))) return true;
+    if (party.isCurrentEndUser) return true;
+    return false;
+  });
+  const nonFavoriteParties = parties.filter((party) => {
+    if (!favoriteActors?.find((actor) => actor?.includes(party.party)) && !party.isCurrentEndUser) return true;
+    return false;
+  });
+  const urnToOrgNr = (urn: string) => {
+    const identifier = 'identifier-no:';
+    const startIndex = urn.indexOf(identifier) + identifier.length;
+    const orgOrPersonNumber = urn.substring(startIndex);
+    if (urn.includes('person')) {
+      return orgOrPersonNumber.slice(0, 6) + ' ' + orgOrPersonNumber.slice(6);
+    }
+    return orgOrPersonNumber?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  };
+
+  const partyFieldFragmentToAccountListItem = (parties: PartyFieldsFragment[]) => {
+    return parties.map((party) => {
+      const isOrganization = party.partyType === 'Organization';
+      const favourite = !!favoriteActors?.find((actor) => actor?.includes(party.party));
+      return {
+        id: party.party,
+        type: party.partyType as AccountListItemType,
+        favourite,
+        title: party.name,
+        label: party.isCurrentEndUser ? 'Deg' : '',
+        favouriteLabel: 'Favoritt',
+        collapsible: true,
+        avatar: {
+          type: isOrganization ? ('company' as AvatarType) : ('person' as AvatarType),
+          name: party.name,
+          size: 'md' as AvatarSize,
+        },
+        description: `${isOrganization ? 'Org.nr. ' : 'Personnummer: '} ${urnToOrgNr(party.party)}`,
+        children: (
+          <Section padding={4} spacing={4}>
+            <Typography>
+              <div className={styles.actorInfoHeader}>{isOrganization ? 'Organisasjonsnummer' : 'Personnummer'}</div>
+              <div className={styles.actorInfoValue}>{urnToOrgNr(party.party)}</div>
+            </Typography>
+            <div>
+              <Typography>
+                <h3>Kontaktinfo:</h3>
+              </Typography>
+              {user && (
+                <DsParagraph style={{ whiteSpace: 'pre-line' }}>
+                  {`${user.party?.person?.firstName} ${user.party?.person?.lastName}
+                  ${user.party?.person?.addressStreetName} ${user.party?.person?.addressHouseNumber}
+                  ${user.party?.person?.addressPostalCode} ${user.party?.person?.addressMunicipalName}`}
+                </DsParagraph>
+              )}
+            </div>
+            <TextField label="Epostadresse for varslinger" size="sm" type="email" value={'ola@nordmann.no'} />
+            <TextField label="SMS-varslinger" size="sm" type="tel" value={'93065211'} />
+            <DsSwitch
+              checked={isPreselectedActor}
+              label="Forhåndsvalgt aktør"
+              value={'alt1'}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setIsPreselectedActor(e.target!.checked)}
+            />
+            <div className={styles.buttonContainer}>
+              <Button color={isOrganization ? ('company' as Color) : ('person' as Color)}>Lagre endringer</Button>
+              <Button variant="outline" color={isOrganization ? ('company' as Color) : ('person' as Color)}>
+                Avbryt
+              </Button>
+            </div>
+          </Section>
+        ),
+        contextMenu: {
+          id: party.party + '-menu',
+          items: [
+            {
+              id: 'inbox',
+              groupId: 'apps',
+              icon: InboxIcon,
+              title: 'Gå til Innboks',
+              onClick: () => navigate(PageRoutes.inbox),
+            },
+            {
+              id: 'fav',
+              groupId: 'context',
+              icon: favourite ? HeartFillIcon : HeartIcon,
+              title: favourite ? 'Fjern fra favoritter' : 'Legg til favoritter',
+              onClick: () => {
+                console.info('Toggling favorite actor', party.party);
+                toggleFavoriteActor(party.party);
+              },
+            },
+            {
+              id: 'new-group',
+              groupId: 'new',
+              icon: PlusIcon,
+              title: 'Ny gruppe',
+            },
+          ],
+          groups: {
+            apps: {
+              title: party.name,
+            },
+          },
+        },
+      };
+    });
+  };
+
   return (
-    <PageBase>
-      <PageNav
-        breadcrumbs={[
-          {
-            label: t('word.frontpage'),
-            href: PageRoutes.inbox,
-          },
-          {
-            label: t('sidebar.profile'),
-            href: PageRoutes.profile,
-          },
-          {
-            label: t('sidebar.profile.actors'),
-            href: PageRoutes.actors,
-          },
-        ]}
-      />
-      <h1>Innstillinger for Mine aktører</h1>
-      <DsAlert className={styles.alert} data-color="info">
-        <Typography>
-          <div className={styles.alertTitle}>På denne siden kan du organisere, favorisere og...</div>
-          Her må vi legge inn utfyllende informasjon om maks antall aktører, favoritter o.l.
-        </Typography>
-      </DsAlert>
-      <div className={styles.searchContainer}>
+    <PageBase color="person">
+      <PageNav breadcrumbs={breadcrumbs} />
+      <Section as="header" spacing={6}>
+        <Heading size="xl">Mine aktører og favoritter</Heading>
         <Toolbar
           search={{
             name: 'actor-search',
@@ -64,106 +193,68 @@ export const Actors = () => {
           }}
           filters={[
             {
-              label: 'Velg avsender',
-              name: 'from',
-              optionType: 'checkbox',
+              name: 'type',
+              label: 'Alle aktører',
+              optionType: 'radio',
               options: [
                 {
-                  label: 'Skatteetaten',
-                  value: 'skatt',
+                  groupId: '1',
+                  label: 'Virksomheter',
+                  value: 'company',
                 },
                 {
-                  label: 'Brønnøysundregistrene',
-                  value: 'brreg',
+                  groupId: '1',
+                  label: 'Personer',
+                  value: 'person',
                 },
                 {
-                  badge: {
-                    label: '10',
-                  },
-                  label: 'Statisitisk sentralbyrå',
-                  value: 'ssb',
+                  groupId: '1',
+                  label: 'Grupper',
+                  value: 'group',
                 },
                 {
-                  badge: {
-                    label: '14',
-                  },
-                  label: 'Fagernes kjøtt og data',
-                  value: 'fkd',
-                },
-                {
-                  badge: {
-                    label: '14',
-                  },
-                  label: 'Direktoratet for vilt-, kjøtt og datautstyr',
-                  value: 'dvkd',
-                },
-                {
-                  label: 'NAV',
-                  value: 'nav',
-                },
-                {
-                  label: 'Oslo kommune',
-                  value: 'oslo',
+                  groupId: '2',
+                  type: 'checkbox',
+                  name: 'deleted',
+                  label: 'Vis slettede enheter',
+                  value: 'true',
                 },
               ],
-              removable: false,
             },
           ]}
-          showResultsLabel="Vis alle treff"
-        >
-          <div>
-            <DsChip.Radio
-              name="underenheter"
-              value="underenheter"
-              checked={showSubActors}
-              data-size="md"
-              onClick={() => setShowSubActors((showSubActors) => !showSubActors)}
-            >
-              Underenheter
-            </DsChip.Radio>
-            <DsChip.Radio
-              name="slettede-aktorer"
-              value="slettede-aktorer"
-              data-size="md"
-              checked={showDeletedActors}
-              onClick={() => setShowDeletedActors(!showDeletedActors)}
-            >
-              Slettede aktører
-            </DsChip.Radio>
-          </div>
-        </Toolbar>
-      </div>
-
-      {favoriteParties.length > 0 && (
-        <div>
-          <h3>{favoriteParties.length > 0 ? 'Mine favoritter' : 'Mine aktører'}</h3>
-          <ListBase>
-            {favoriteParties.map((party) => (
-              <PartyListItem
-                key={party.party}
-                party={party}
-                forceExpand={showSubActors}
-                isFavorite={!!favoriteActors?.find((actor) => actor?.includes(party.party))}
-              />
-            ))}
-          </ListBase>
-        </div>
-      )}
-      {nonFavoriteParties.length > 0 && (
-        <div>
-          <h3>{favoriteParties.length > 0 ? 'Øvrige aktører (A-Å)' : 'Mine aktører'}</h3>
-          <ListBase>
-            {nonFavoriteParties.map((party) => (
-              <PartyListItem
-                key={party.party}
-                party={party}
-                forceExpand={showSubActors}
-                isFavorite={!!favoriteActors?.find((actor) => actor?.includes(party.party))}
-              />
-            ))}
-          </ListBase>
-        </div>
-      )}
+        />
+      </Section>
+      <Section spacing={6}>
+        {searchValue ? (
+          <>
+            <Heading size="lg">Søkeresultater:</Heading>
+            <ListBase>
+              {partyFieldFragmentToAccountListItem(parties).map((party) => (
+                <PartyListItem key={party.id} party={party} />
+              ))}
+            </ListBase>
+          </>
+        ) : (
+          <>
+            <Heading size="lg">Deg selv og favoritter</Heading>
+            <ListBase>
+              {partyFieldFragmentToAccountListItem(favoriteParties).map((party) => (
+                <PartyListItem key={party.id} party={party} />
+              ))}
+            </ListBase>
+          </>
+        )}
+        {nonFavoriteParties.length > 0 && !searchValue && (
+          <>
+            <Heading size="lg">Andre aktører</Heading>
+            <ListBase>
+              {partyFieldFragmentToAccountListItem(nonFavoriteParties).map((party) => (
+                <PartyListItem key={party.id} party={party} />
+              ))}
+            </ListBase>
+          </>
+        )}
+      </Section>
     </PageBase>
   );
 };
--- packages/frontend/src/profile/useProfile.tsx
+++ packages/frontend/src/profile/useProfile.tsx
@@ -1,8 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import type { ProfileQuery, User } from 'bff-types-generated';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { profile } from '../api/queries.ts';
+import { addFavoriteActor, deleteFavoriteActor, profile } from '../api/queries.ts';
 import { QUERY_KEYS } from '../constants/queryKeys.ts';
 
 export const useProfile = () => {
@@ -13,17 +13,36 @@ export const useProfile = () => {
   });
   const { i18n } = useTranslation();
   const language = data?.profile?.language || i18n.language || 'nb';
+  const favoriteActors = data?.profile?.favoriteActors;
+  const queryClient = useQueryClient();
+
   // biome-ignore lint/correctness/useExhaustiveDependencies: Full control of what triggers this code is needed
   useEffect(() => {
     if (language !== i18n.language) {
       void i18n.changeLanguage(language);
     }
   }, [language]);
+
+  const toggleFavoriteActor = (party: string) => {
+    const isFavorite = !!favoriteActors?.find((actor) => actor?.includes(party));
+
+    if (isFavorite) {
+      deleteFavoriteActor(party).then(() => {
+        void queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.PROFILE] });
+      });
+    } else {
+      addFavoriteActor(party).then(() => {
+        void queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.PROFILE] });
+      });
+    }
+  };
+
   return {
     profile: data?.profile,
     isLoading,
     language,
     user: data?.profile?.user as User,
-    favoriteActors: data?.profile?.favoriteActors,
+    favoriteActors,
+    toggleFavoriteActor,
   };
 };
