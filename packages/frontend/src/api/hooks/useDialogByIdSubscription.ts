65e77e67df8923ff0f0dd3364bcbd2fb1514574d
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-05-05 16:20:46 +0200

    fix: keep query params when navigated to inbox upon dialog delete (#2118)

--- packages/frontend/src/api/hooks/useDialogByIdSubscription.ts
+++ packages/frontend/src/api/hooks/useDialogByIdSubscription.ts
@@ -1,14 +1,16 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { DialogEventType } from 'bff-types-generated';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { SSE } from 'sse.js';
 import { QUERY_KEYS } from '../../constants/queryKeys.ts';
+import { pruneSearchQueryParams } from '../../pages/Inbox/queryParams.ts';
 import { PageRoutes } from '../../pages/routes.ts';
 
 export const useDialogByIdSubscription = (dialogId: string | undefined, dialogToken: string | undefined) => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const { search } = useLocation();
 
   // biome-ignore lint: lint/correctness/useExhaustiveDependencies
   useEffect(() => {
@@ -28,7 +30,7 @@ export const useDialogByIdSubscription = (dialogId: string | undefined, dialogTo
         const updatedType: DialogEventType | undefined = jsonPayload.data?.dialogEvents?.type;
         if (updatedType && updatedType === DialogEventType.DialogDeleted) {
           // Redirect to inbox if the dialog was deleted
-          navigate(PageRoutes.inbox);
+          navigate(PageRoutes.inbox + pruneSearchQueryParams(search.toString()));
         } else if (updatedType && updatedType === DialogEventType.DialogUpdated) {
           void queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.DIALOG_BY_ID] });
           void queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.DIALOGS] });
