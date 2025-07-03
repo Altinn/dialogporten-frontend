548eab6e03a28fbbda24e36f9318e40d4328c744
Alexander Midteide <alexander.midteide@digdir.no>
2025-07-03 14:48:45 +0200

    feat: Added filters to PartiesOverview and added Feature Toggle

--- packages/frontend/src/featureFlags/FeatureFlags.ts
+++ packages/frontend/src/featureFlags/FeatureFlags.ts
@@ -1,14 +1,17 @@
 export enum FeatureFlagKeys {
   EnableDebugHeaderScreen = 'EnableDebugHeaderScreen',
   DisableBulkActions = 'DisableBulkActions',
+  DisableFavoriteGroups = 'DisableFavoriteGroups',
 }
 
 export interface FeatureFlags {
   [FeatureFlagKeys.EnableDebugHeaderScreen]: boolean;
   [FeatureFlagKeys.DisableBulkActions]: boolean;
+  [FeatureFlagKeys.DisableFavoriteGroups]: boolean;
 }
 
 export const featureFlags: FeatureFlags = {
   [FeatureFlagKeys.EnableDebugHeaderScreen]: true,
   [FeatureFlagKeys.DisableBulkActions]: true,
+  [FeatureFlagKeys.DisableFavoriteGroups]: true,
 };
