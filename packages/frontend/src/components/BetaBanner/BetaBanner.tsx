fd5f0893dbfc1950721aae3463732fad4038361a
Alexander Midteide <alexander.midteide@digdir.no>
2025-07-15 14:14:53 +0200

    feat: Banner now without close/hide button. Updated text with translations.

--- packages/frontend/src/components/BetaBanner/BetaBanner.tsx
+++ packages/frontend/src/components/BetaBanner/BetaBanner.tsx
@@ -1,28 +1,14 @@
 import { Banner } from '@altinn/altinn-components';
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 export const BetaBanner = () => {
-  const betaBannerKey = 'arbeidsflate:show_beta_banner';
-  const isPreviouslyDismissed = localStorage.getItem(betaBannerKey) === 'true';
-  const [showBetaBanner, setShowBetaBanner] = useState<boolean>(!isPreviouslyDismissed);
-
   const { t } = useTranslation();
-
-  const onClose = () => {
-    setShowBetaBanner(false);
-    localStorage.setItem(betaBannerKey, 'true');
-  };
-
-  if (!showBetaBanner) {
-    return null;
-  }
 
   return (
     <Banner
-      onClose={onClose}
-      text="Du ser nå på en beta-versjon av nye Altinn Innboks i et testmiljø. Alt innhold du ser her er basert på
-          testdata og kun ment for demonstrasjon."
+      // biome-ignore lint/a11y/useValidAnchor:
+      // biome-ignore lint/a11y/useAnchorContent:
+      text={<Trans i18nKey="beta.banner" components={{ span: <span />, a: <a /> }} />}
       color="warning"
       closeTitle={t('word.close')}
     />
