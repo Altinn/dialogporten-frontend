e5aca7fd8105c257564dc683098215262efef87b
Mateusz Bacherycz <mbacherycz@icloud.com>
2025-08-12 12:23:26 +0200

    chore: add e2e smoke tests

---
+++ packages/e2e/tests/inbox-smoke.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@playwright/test';
+import { loginUser } from './utils/auth';
+
+test.describe('Inbox smoke tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await loginUser(page);
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
+
+  test('should load inbox correctly', async ({ page, baseURL }) => {
+    await expect(page).toHaveURL(`${baseURL}/`);
+  });
+
+  test('should fetch dialogs and open correctly', async ({ page, baseURL }) => {
+    await expect(page.getByRole('link', { name: 'Testdriver for Arbeidsflate' })).toBeVisible();
+    await page.getByRole('link', { name: 'Testdriver for Arbeidsflate' }).click();
+    await expect(page.getByRole('button', { name: 'Gå til skjemautfylling' })).toBeVisible();
+    await expect(page).toHaveURL(`${baseURL}/inbox/0197abb7-bf17-70f9-84d3-c7bee2d96612/`);
+  });
+});
