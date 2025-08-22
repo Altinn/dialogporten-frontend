56aeb3ff043cf358b5d446437af26263ebae9ec0
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-08-22 14:27:25 +0200

    fix: sort order for activity log + url in homepage redirect (#2532)

--- packages/frontend/src/api/utils/activities.tsx
+++ packages/frontend/src/api/utils/activities.tsx
@@ -150,6 +150,6 @@ export const getActivityHistory = ({
   }));
 
   return [...dialogHistoryActivities, ...dialogHistoryTransmissions].sort((a, b) => {
-    return new Date(a.date).getTime() - new Date(b.date).getTime();
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
   });
 };
