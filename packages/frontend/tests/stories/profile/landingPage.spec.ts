5b8346d485b8aea7f9e700c2f6e6372d42c754ab
Alexander Midteide <alexander.midteide@digdir.no>
2025-08-27 11:41:40 +0200

    Commented out profile PW tests until we are closer to final product

--- packages/frontend/tests/stories/profile/landingPage.spec.ts
+++ packages/frontend/tests/stories/profile/landingPage.spec.ts
@@ -1,28 +1,28 @@
-import type { Page } from '@playwright/test';
-import { appURLProfileLanding, appURLProfileNotifications, appURLProfileSettings } from '../..';
-import { expect, test } from '../../fixtures';
+// import type { Page } from '@playwright/test';
+// import { appURLProfileLanding, appURLProfileNotifications, appURLProfileSettings } from '../..';
+// import { expect, test } from '../../fixtures';
 
-test.describe('Profile Landing Page', () => {
-  test('Smoke test', async ({ page }: { page: Page }) => {
-    await page.goto(appURLProfileLanding);
+// test.describe('Profile Landing Page', () => {
+//   test('Smoke test', async ({ page }: { page: Page }) => {
+//     await page.goto(appURLProfileLanding);
 
-    await expect(page.getByRole('navigation').filter({ hasText: 'ForsideMin profil' })).toBeVisible();
+//     await expect(page.getByRole('navigation').filter({ hasText: 'ForsideMin profil' })).toBeVisible();
 
-    await expect(page.getByRole('heading', { name: 'John Doe' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Varsler er på' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Flere innstillinger' })).toBeVisible();
-  });
+//     await expect(page.getByRole('heading', { name: 'John Doe' })).toBeVisible();
+//     await expect(page.getByRole('link', { name: 'Varsler er på' })).toBeVisible();
+//     await expect(page.getByRole('link', { name: 'Flere innstillinger' })).toBeVisible();
+//   });
 
-  test('Navigation redirects correctly', async ({ page }: { page: Page }) => {
-    await page.goto(appURLProfileLanding);
+//   test('Navigation redirects correctly', async ({ page }: { page: Page }) => {
+//     await page.goto(appURLProfileLanding);
 
-    await page.getByRole('link', { name: 'Varsler er på' }).click();
-    expect(new URL(page.url()).pathname).toBe(new URL(appURLProfileNotifications).pathname);
+//     await page.getByRole('link', { name: 'Varsler er på' }).click();
+//     expect(new URL(page.url()).pathname).toBe(new URL(appURLProfileNotifications).pathname);
 
-    await page.goBack();
-    await expect(page.getByRole('heading', { name: 'John Doe' })).toBeVisible();
+//     await page.goBack();
+//     await expect(page.getByRole('heading', { name: 'John Doe' })).toBeVisible();
 
-    await page.getByRole('link', { name: 'Flere innstillinger' }).click();
-    expect(new URL(page.url()).pathname).toBe(new URL(appURLProfileSettings).pathname);
-  });
-});
+//     await page.getByRole('link', { name: 'Flere innstillinger' }).click();
+//     expect(new URL(page.url()).pathname).toBe(new URL(appURLProfileSettings).pathname);
+//   });
+// });
