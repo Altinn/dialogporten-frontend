95c7766e8e59dc4f7e74a79ce015b8344a05aecb
Mateusz Bacherycz <mbacherycz@icloud.com>
2025-09-01 13:19:32 +0200

    fix: beta modal navigation to onboarding missing ctx

--- packages/frontend/src/components/BetaModal/BetaModal.tsx
+++ packages/frontend/src/components/BetaModal/BetaModal.tsx
@@ -2,9 +2,10 @@ import { Button, ButtonGroup, FloatingActionButton, Modal, Typography } from '@a
 import { InformationSquareIcon } from '@navikt/aksel-icons';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { createMessageBoxLink } from '../../auth';
 import { QUERY_KEYS } from '../../constants/queryKeys.ts';
+import { pruneSearchQueryParams } from '../../pages/Inbox/queryParams.ts';
 import { PageRoutes } from '../../pages/routes.ts';
 import { useGlobalState } from '../../useGlobalState.ts';
 import styles from './betaModal.module.css';
@@ -19,12 +20,13 @@ export const BetaModal = () => {
   const isFirstTime = localStorage.getItem(betaKey) === null;
   const [isOpen, setIsOpen] = useState<boolean>(isFirstTime);
   const [_, setShowTour] = useGlobalState<boolean>(QUERY_KEYS.SHOW_TOUR, false);
+  const { search } = useLocation();
 
   const onTryBeta = () => {
     localStorage.setItem(betaKey, 'true');
     setIsOpen(false);
     setShowTour(true);
-    navigate(PageRoutes.inbox);
+    navigate(PageRoutes.inbox + pruneSearchQueryParams(search));
   };
 
   if (isMock) return null;
@@ -43,7 +45,11 @@ export const BetaModal = () => {
           </Button>
         </ButtonGroup>
         <Typography size="sm">
-          <Link target="_blank" to={PageRoutes.about} onClick={() => localStorage.setItem(betaKey, 'true')}>
+          <Link
+            target="_blank"
+            to={PageRoutes.about + pruneSearchQueryParams(search)}
+            onClick={() => localStorage.setItem(betaKey, 'true')}
+          >
             {t('betaModal.learnMore')}
           </Link>
         </Typography>
