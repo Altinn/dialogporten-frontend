13ca967e895fe02e66b3fd90925fb1de8cf075c3
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-09-12 10:56:40 +0200

    fix: tour not showing

--- packages/frontend/src/useGlobalState.ts
+++ packages/frontend/src/useGlobalState.ts
@@ -1,21 +1,47 @@
-import { useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
+import type { QUERY_KEYS } from './constants/queryKeys.ts';
 
-export function useGlobalState<T>(queryKey: string, defaultValue: T): [T, (newValue: T) => void] {
+export function useGlobalStringState(queryKey: string, defaultValue: string): [string, (newValue: string) => void] {
   const queryClient = useQueryClient();
 
   // seed from cache if present, else from default
-  const cached = queryClient.getQueryData<T>([queryKey]);
-  const [local, setLocal] = useState<T>(cached ?? defaultValue);
+  const cached = queryClient.getQueryData<string>([queryKey]);
+  const [local, setLocal] = useState<string>(cached ?? defaultValue);
 
   useEffect(() => {
     setValue(local);
   }, [local]);
 
-  const setValue = (newValue: T) => {
+  const setValue = (newValue: string) => {
     setLocal(newValue);
     queryClient.setQueryData([queryKey], newValue);
   };
-
   return [local, setValue];
+}
+
+type UseGlobalStateReturn<T> = [T, (newValue: T) => void];
+
+export function useGlobalState<T>(
+  queryKey: (typeof QUERY_KEYS)[keyof typeof QUERY_KEYS],
+  defaultValue: T,
+): UseGlobalStateReturn<T> {
+  const queryClient = useQueryClient();
+
+  const { data = defaultValue } = useQuery<T>({
+    queryKey: [queryKey],
+    staleTime: Number.POSITIVE_INFINITY,
+    enabled: false,
+    initialData: defaultValue,
+    queryFn: async () => defaultValue,
+  });
+
+  const { mutate } = useMutation<T, unknown, T>({
+    mutationFn: async (newValue) => newValue,
+    onMutate: (newValue) => {
+      queryClient.setQueryData([queryKey], newValue);
+    },
+  });
+
+  return [data, mutate];
 }
