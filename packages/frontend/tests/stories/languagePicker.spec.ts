64de38f625ff7a4ba725f9fa3506bb4731c6c1e7
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-10-01 09:58:26 +0200

    fix: update to latest version of altinn-components 0.41.0 (#2823)

--- packages/frontend/tests/stories/languagePicker.spec.ts
+++ packages/frontend/tests/stories/languagePicker.spec.ts
@@ -6,10 +6,7 @@ test.describe('Language picker', () => {
     await page.goto(defaultAppURL);
 
     const listItem = page.getByRole('listitem').filter({ hasText: 'Melding om bortkjøring av snø' });
-    const timeElement = listItem.locator('time').filter({
-      hasText: 'Oslo kommune',
-    });
-    await expect(timeElement).toHaveText('Oslo kommune til Test Testesen');
+    await expect(listItem).toContainText('Oslo kommune til Test Testesen');
 
     await expect(page.getByTestId('searchbar-input')).toHaveAttribute('placeholder', 'Søk');
     await expect(page.getByRole('button', { name: 'add' })).toHaveText('Legg til filter');
@@ -40,10 +37,7 @@ test.describe('Language picker', () => {
 
     await expect(page.getByTestId('searchbar-input')).toHaveAttribute('placeholder', 'Search');
     await expect(page.getByRole('button', { name: 'add' })).toHaveText('Add filter');
-    const timeElementEn = listItem.locator('time').filter({
-      hasText: 'to Test Testesen',
-    });
-    await expect(timeElementEn).toContainText('City of Oslo to Test Testesen');
+    await expect(listItem).toContainText('City of Oslo to Test Testesen');
 
     if (isMobile) {
       await page.getByRole('button', { name: 'Menu' }).click();
