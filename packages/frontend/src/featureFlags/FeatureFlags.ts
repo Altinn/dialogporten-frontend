7d8326d623026abd750c99f22854e076df53bd39
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-10-02 16:02:28 +0200

    fix: improvements to filtering party types in party overview (#2849)

--- packages/frontend/src/featureFlags/FeatureFlags.ts
+++ packages/frontend/src/featureFlags/FeatureFlags.ts
@@ -1,7 +1,6 @@
 export enum FeatureFlagKeys {
   EnableDebugHeaderScreen = 'EnableDebugHeaderScreen',
   DisableBulkActions = 'DisableBulkActions',
-  DisableFavoriteGroups = 'DisableFavoriteGroups',
   EnableProfilePages = 'EnableProfilePages',
   EnableProfileLinkInGlobalMenu = 'EnableProfileLinkInGlobalMenu',
   EnableAccessManagementLinkInGlobalMenu = 'EnableAccessManagementLinkInGlobalMenu',
@@ -10,7 +9,6 @@ export enum FeatureFlagKeys {
 export interface FeatureFlags {
   [FeatureFlagKeys.EnableDebugHeaderScreen]: boolean;
   [FeatureFlagKeys.DisableBulkActions]: boolean;
-  [FeatureFlagKeys.DisableFavoriteGroups]: boolean;
   [FeatureFlagKeys.EnableProfilePages]: boolean;
   [FeatureFlagKeys.EnableProfileLinkInGlobalMenu]: boolean;
   [FeatureFlagKeys.EnableAccessManagementLinkInGlobalMenu]: boolean;
@@ -19,7 +17,6 @@ export interface FeatureFlags {
 export const featureFlags: FeatureFlags = {
   [FeatureFlagKeys.EnableDebugHeaderScreen]: true,
   [FeatureFlagKeys.DisableBulkActions]: true,
-  [FeatureFlagKeys.DisableFavoriteGroups]: true,
   [FeatureFlagKeys.EnableProfilePages]: !location.hostname.includes('af.altinn.no'),
   [FeatureFlagKeys.EnableProfileLinkInGlobalMenu]:
     import.meta.env.DEV || location.hostname.includes('af.at.altinn.cloud'),
