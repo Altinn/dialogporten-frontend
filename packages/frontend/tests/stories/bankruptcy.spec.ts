12e6b40cd2248f85de661d12531f298f3a77446b
Mateusz Bacherycz <mbacherycz@icloud.com>
2025-10-17 09:31:24 +0200

    fix: Remove bankruptcy group title and description

--- packages/frontend/tests/stories/bankruptcy.spec.ts
+++ packages/frontend/tests/stories/bankruptcy.spec.ts
@@ -6,17 +6,12 @@ test.describe('Bankruptcy dialogs group', () => {
   test('Bankruptcy group NOT present if no bankruptcy dialogs', async ({ page }) => {
     await page.goto(defaultAppURL);
 
-    await expect(page.getByRole('heading', { name: 'Konkurssaker' })).not.toBeVisible();
-    await expect(page.getByText('Konkurssaker krever spesiell')).not.toBeVisible();
     await expect(page.getByRole('link', { name: 'Bankruptcy title 1' })).not.toBeVisible();
     await expect(page.getByRole('link', { name: 'Bankruptcy title 2' })).not.toBeVisible();
   });
 
   test('Bankruptcy group present with bankruptcy dialogs', async ({ page }) => {
     await page.goto(appUrlWithPlaywrightId('bankruptcy'));
-
-    await expect(page.getByRole('heading', { name: 'Konkurssaker' })).toBeVisible();
-    await expect(page.getByText('Konkurssaker krever spesiell')).toBeVisible();
 
     await expect(page.getByRole('link', { name: 'Bankruptcy title 1' })).toBeVisible();
     await expect(page.getByRole('link', { name: 'Bankruptcy title 2' })).toBeVisible();
