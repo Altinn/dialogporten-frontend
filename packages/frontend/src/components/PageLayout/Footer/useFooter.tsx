8e9c11cc4ca0155d2c53726bd178cefdc30e19ce
Mateusz Marek Bacherycz <mbacherycz@icloud.com>
2025-11-17 16:01:28 +0100

    fix: Update info portal links with language (#3224)

--- packages/frontend/src/components/PageLayout/Footer/useFooter.tsx
+++ packages/frontend/src/components/PageLayout/Footer/useFooter.tsx
@@ -4,9 +4,9 @@ import { useParties } from '../../../api/hooks/useParties';
 import { getFooterLinks } from '../../../auth/url';
 
 export const useFooter = (): FooterProps => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const { currentPartyUuid } = useParties();
-  const footerLinks = getFooterLinks(currentPartyUuid || '');
+  const footerLinks = getFooterLinks(currentPartyUuid || '', i18n.language);
 
   return {
     address: 'Postboks 1382 Vika, 0114 Oslo.',
