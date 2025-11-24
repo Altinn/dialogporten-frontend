a7df86ae5c6f61d3c9ff8c4cdbcce4e87d91b445
Alexander Midteide <alexander.midteide@digdir.no>
2025-11-24 11:43:03 +0100

    fix: Altinn 2 active elements notification now behind feature flag

--- packages/frontend/src/api/hooks/useAltinn2Messages.tsx
+++ packages/frontend/src/api/hooks/useAltinn2Messages.tsx
@@ -1,19 +1,21 @@
 import type { Altinn2messagesQuery } from 'bff-types-generated';
 import { useAuthenticatedQuery } from '../../auth/useAuthenticatedQuery.tsx';
 import { QUERY_KEYS } from '../../constants/queryKeys.ts';
+import { useFeatureFlag } from '../../featureFlags/useFeatureFlag.ts';
 import { fetchAltinn2Messages } from '../queries.ts';
 
 export const useAltinn2Messages = (selectedAccountIdentifier?: string) => {
+  const isAltinn2MessagesEnabled = useFeatureFlag('inbox.enableAltinn2Messages') as boolean;
   const { data, isLoading, isSuccess } = useAuthenticatedQuery<Altinn2messagesQuery>({
     queryKey: [QUERY_KEYS.ALTINN2_MESSAGES, selectedAccountIdentifier],
     queryFn: () => fetchAltinn2Messages(selectedAccountIdentifier ?? ''),
     retry: 3,
     staleTime: 1000 * 60 * 10,
-    enabled: !!selectedAccountIdentifier,
+    enabled: !!selectedAccountIdentifier && isAltinn2MessagesEnabled,
   });
 
   return {
-    altinn2messages: data?.altinn2messages,
+    altinn2messages: data?.altinn2messages ?? [],
     isLoading,
     isSuccess,
   };
