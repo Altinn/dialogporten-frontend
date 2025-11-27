1fca2836ad03944e685494e9c398306fdd99b097
Alexander Midteide <alexander.midteide@digdir.no>
2025-11-27 11:27:40 +0100

    feat: Added link to notification settings

--- packages/frontend/src/pages/Profile/NotificationsPage/NotificationsPage.tsx
+++ packages/frontend/src/pages/Profile/NotificationsPage/NotificationsPage.tsx
@@ -1,13 +1,14 @@
-import { Breadcrumbs, Heading, PageBase, SettingsList, Toolbar } from '@altinn/altinn-components';
+import { Breadcrumbs, Heading, ListItemLabel, PageBase, SettingsList, Toolbar } from '@altinn/altinn-components';
 import type { NotificationSettingsResponse, PartyFieldsFragment } from 'bff-types-generated';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 import { useParties } from '../../../api/hooks/useParties.ts';
 import { usePageTitle } from '../../../hooks/usePageTitle';
+import { getIsProdEnvironment } from '../Settings/ContactProfileDetails.tsx';
 import { getBreadcrumbs } from '../Settings/Settings.tsx';
 import { SettingsType, useSettings } from '../Settings/useSettings.tsx';
 import { useProfile } from '../useProfile';
-
+import styles from './notificationsPage.module.css';
 export interface NotificationAccountsType extends PartyFieldsFragment {
   notificationSettings?: NotificationSettingsResponse;
   parentId?: string;
@@ -18,8 +19,25 @@ export const NotificationsPage = () => {
   const { search } = useLocation();
   const { isLoading: isLoadingUser } = useProfile();
   const { isLoading: isLoadingParties } = useParties();
+  const isProdEnvironment = getIsProdEnvironment();
+  const notificationSettingsUrl = isProdEnvironment
+    ? 'https://info.altinn.no/hjelp/dette-jobber-vi-fortsatt-med/profil--og-varslingsinnstillinger/'
+    : 'https://inte.altinn.cloud/hjelp/dette-jobber-vi-fortsatt-med/profil--og-varslingsinnstillinger/';
 
   usePageTitle({ baseTitle: t('component.notifications') });
+
+  const companiesTitle = (
+    <div className={styles.companiesTitle}>
+      <ListItemLabel title={t('profile.settings.company_notifications')}>
+        {t('profile.settings.company_notifications')}
+      </ListItemLabel>
+      <span className={styles.infoText}>
+        <a className={styles.link} href={notificationSettingsUrl}>
+          {t('profile.settings.where_individual_services_notifications')}
+        </a>
+      </span>
+    </div>
+  );
 
   const { settingsGroups, settings, settingsSearch } = useSettings({
     isLoading: isLoadingUser || isLoadingParties,
@@ -27,16 +45,16 @@ export const NotificationsPage = () => {
       excludeGroups: [SettingsType.contact, SettingsType.primary, SettingsType.favorites],
       groups: {
         [SettingsType.alerts]: {
-          title: 'Varslingsadresser',
+          title: t('profile.settings.notification_addresses'),
         },
         [SettingsType.profiles]: {
-          title: 'Varslingsprofiler',
+          title: t('profile.settings.notification_profiles'),
         },
         [SettingsType.persons]: {
-          title: 'Varslinger for andre personer',
+          title: t('profile.settings.person_notifications'),
         },
         [SettingsType.companies]: {
-          title: 'Varslinger for virksomheter',
+          title: companiesTitle,
         },
       },
     },
