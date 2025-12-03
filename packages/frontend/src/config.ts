45a5b352b38287f2bebc64c22815c03e65cfb5cb
Are Almaas <arealmaas@gmail.com>
2025-12-03 13:46:28 +0100

    feat(frontend): add environment variable for dialogporten stream url

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -7,5 +7,6 @@ declare const window: Window;
 
 export const config = {
   applicationInsightsInstrumentationKey: window.applicationInsightsInstrumentationKey,
-  dialogportenStreamUrl: window.dialogportenStreamUrl,
+  dialogportenStreamUrl:
+    window.dialogportenStreamUrl || 'https://altinn-dev-api.azure-api.net/dialogporten/graphql/stream',
 };
