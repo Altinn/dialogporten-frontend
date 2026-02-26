2f2ad2dd0cba14974417371036dea7c5a1884ad5
Sean Erik Scully <sean.erik.scully@gmail.com>
2026-02-26 21:18:55 +0100

    fix: disable fetching a2 messages for SI users since this always will result in 401

--- packages/frontend/src/api/hooks/useAltinn2Messages.tsx
+++ packages/frontend/src/api/hooks/useAltinn2Messages.tsx
@@ -3,15 +3,13 @@ import { useAuthenticatedQuery } from '../../auth/useAuthenticatedQuery.tsx';
 import { QUERY_KEYS } from '../../constants/queryKeys.ts';
 import { useFeatureFlag } from '../../featureFlags';
 import { fetchAltinn2Messages } from '../queries.ts';
-import { useParties } from './useParties.ts';
 
 export const useAltinn2Messages = (selectedAccountIdentifier?: string) => {
-  const { isSelfIdentifiedUser } = useParties();
-  const enabled =
-    useFeatureFlag<boolean>('inbox.enableAltinn2Messages') && (isSelfIdentifiedUser || !!selectedAccountIdentifier);
+  /* Note: Upstream A2 API does not support self-identified users */
+  const enabled = useFeatureFlag<boolean>('inbox.enableAltinn2Messages') && !!selectedAccountIdentifier;
   const { data, isLoading, isSuccess } = useAuthenticatedQuery<Altinn2messagesQuery>({
     queryKey: [QUERY_KEYS.ALTINN2_MESSAGES, selectedAccountIdentifier],
-    queryFn: () => fetchAltinn2Messages(selectedAccountIdentifier ?? '', isSelfIdentifiedUser),
+    queryFn: () => fetchAltinn2Messages(selectedAccountIdentifier ?? '', false),
     retry: 3,
     staleTime: 1000 * 60 * 10,
     enabled,
