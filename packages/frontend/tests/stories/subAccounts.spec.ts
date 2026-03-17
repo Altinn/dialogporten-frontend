81d25548128047ae60fa9c123447ef229da4eb80
Sean Erik Scully <sean.erik.scully@gmail.com>
2026-03-17 15:00:40 +0100

    feat: support saving search with only subunits applied (#3867)

--- packages/frontend/tests/stories/subAccounts.spec.ts
+++ packages/frontend/tests/stories/subAccounts.spec.ts
@@ -27,7 +27,7 @@ test.describe('Multiselect subaccounts', () => {
     await page.getByRole('button', { name: 'Alle enheter' }).click();
     await page.getByLabel('Testbedrift As Avd Sub', { exact: true }).click();
     await page.getByLabel('Testbedrift As Avd Sub ↳ Org').press('Escape');
-    await expect(page.getByRole('heading', { name: 'Innboksen er tom' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Ingen treff' })).toBeVisible();
 
     /* Add a subunit with dialog */
     await page.getByRole('button', { name: 'Testbedrift As Avd Sub' }).click();
