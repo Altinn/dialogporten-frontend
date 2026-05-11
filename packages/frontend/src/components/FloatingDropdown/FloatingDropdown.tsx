39e0673b11a4e36e07bcbb2ba80a5d4ef761101a
Mateusz Marek Bacherycz <mbacherycz@icloud.com>
2026-05-11 12:55:35 +0200

    feat: Replace ChangeReporteeAndRedirect endpoint from A2 to A3 (#4104)
    * feat: Replace ChangeReporteeAndRedirect endpoint from A2 to A3
    
    * fix PR comments, restore currentPartyUuid for url build

--- packages/frontend/src/components/FloatingDropdown/FloatingDropdown.tsx
+++ packages/frontend/src/components/FloatingDropdown/FloatingDropdown.tsx
@@ -1,7 +1,7 @@
 import { FloatingDropdown as FloatingDropdownAc } from '@altinn/altinn-components';
 import { ExternalLinkIcon, LeaveIcon, QuestionmarkIcon } from '@navikt/aksel-icons';
 import { useTranslation } from 'react-i18next';
-import { useCurrentPartyUuid } from '../../api/hooks/usePartiesSelectors';
+import { useCurrentPartyUuid } from '../../api/hooks/usePartiesSelectors.ts';
 import { createMessageBoxLink, getNeedHelpLink } from '../../auth';
 import { i18n } from '../../i18n/config';
 
@@ -14,7 +14,7 @@ export const FloatingDropdown = () => {
   };
 
   const handleGoToHelp = () => {
-    window.location.href = getNeedHelpLink(currentPartyUuid, i18n.language);
+    window.location.href = getNeedHelpLink(i18n.language);
   };
 
   const items = [
