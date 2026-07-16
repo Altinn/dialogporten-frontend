3e2255600eb1c24407fc7ac2079228763e559fd2
Sean Erik Scully <sean.erik.scully@gmail.com>
2026-07-16 15:07:11 +0200

    chore(test): add Postgres integration smoke tests for saved searches

--- packages/bff/src/db.ts
+++ packages/bff/src/db.ts
@@ -8,29 +8,26 @@ export let PartyRepository: Repository<Party> | undefined = undefined;
 
 export let dataSource: DataSource | undefined = undefined;
 
+export const initRepositories = (initializedDataSource: DataSource) => {
+  dataSource = initializedDataSource;
+
+  GroupRepository = initializedDataSource.getRepository(Group);
+  ProfileRepository = initializedDataSource.getRepository(ProfileTable);
+  SavedSearchRepository = initializedDataSource.getRepository(SavedSearch);
+  PartyRepository = initializedDataSource.getRepository(Party);
+
+  return {
+    GroupRepository,
+    PartyRepository,
+    ProfileRepository,
+    SavedSearchRepository,
+    dataSource: initializedDataSource,
+  };
+};
+
 export const connectToDB = async () => {
   // we don't want to import data-source.ts in the initial import, because it loads config.ts
   // which is not needed for example for generating bff-types
   const { connectionOptions } = await import('./data-source.ts');
-  dataSource = await new DataSource(connectionOptions).initialize();
-
-  GroupRepository = dataSource.getRepository(Group);
-  ProfileRepository = dataSource.getRepository(ProfileTable);
-  SavedSearchRepository = dataSource.getRepository(SavedSearch);
-  PartyRepository = dataSource.getRepository(Party);
-
-  if (!GroupRepository) {
-    throw new Error('GroupRepository not initialized');
-  }
-  if (!PartyRepository) {
-    throw new Error('PartyRepository not initialized');
-  }
-  if (!ProfileRepository) {
-    throw new Error('ProfileRepository not initialized');
-  }
-  if (!SavedSearchRepository) {
-    throw new Error('SavedSearchRepository not initialized');
-  }
-
-  return { ProfileRepository, SavedSearchRepository, dataSource };
+  return initRepositories(await new DataSource(connectionOptions).initialize());
 };
